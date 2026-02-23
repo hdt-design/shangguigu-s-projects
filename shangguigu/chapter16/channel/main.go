ab8e0cd525c12f78845418d4aa184eec025903cf
liuxinpei <2512827903@qq.com>
2026-02-23 11:42:14 +0800

    channel存放int,map,结构体,结构体指针类型

--- shangguigu/chapter16/channel/main.go
+++ shangguigu/chapter16/channel/main.go
@@ -23,7 +23,7 @@ func main() {
 	num3 := <-intChan
 	fmt.Printf("num1=%d,num2=%d,num3=%d\n", num1, num2, num3)
 
-	/*-----------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------*/
 
 	//使用channel存放map类型
 	var mapChan chan map[string]string
@@ -40,7 +40,7 @@ func main() {
 	mapChan <- m2
 	fmt.Println("mapChan=", mapChan)
 
-	/*----------------------------------------------------------------------------*/
+	/*-------------------------------------------------------------------------*/
 
 	//使用channel存放结构体类型
 	var catChan chan Cat
@@ -57,4 +57,20 @@ func main() {
 	cat11 := <-catChan
 	cat22 := <-catChan
 	fmt.Println("cat11=", cat11, "cat22=", cat22)
+
+	/*-----------------------------------------------------------------------*/
+
+	//存放结构体指针
+	var catChan2 chan *Cat
+	catChan2 = make(chan *Cat, 10)
+
+	cat1 = Cat{Name: "tom", Age: 18}
+	cat2 = Cat{Name: "tom~", Age: 180}
+	catChan2 <- &cat1
+	catChan2 <- &cat2
+
+	cat3 := <-catChan2
+	cat4 := <-catChan2
+	fmt.Println("catChan2=", catChan2)
+	fmt.Println(cat3, cat4)
 }
